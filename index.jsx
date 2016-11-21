93966d584e9242bf1ad24879b4482dd64f480e67
Maxwell Simmer <maxwell.simmer@gmail.com>
2016-11-22 00:20:25 +0100

    Add static class methods; Add flags option; Check for required options

--- index.jsx
+++ index.jsx
@@ -4,81 +4,87 @@ import { exec } from 'child_process'
 
 class Epub {
   constructor() {
-
-    this.input = null
-    this.output = null
-    this.clean = null
-    this.modified = null
-    this.bookname = null
-    this.bookpath = null
-
-    this._set = function _set(key, val) {
-      this[key] = val
-      return this[key]
+    this.options = {
+      input: null,
+      output: null,
+      bookname: null,
+      clean: true,
+      flags: ['-e']
     }
+  }
 
-    this._get = function _get(key) {
-      return this[key]
-    }
+  _set(key, val) {
+    this.options[key] = val
+    return this[key]
+  }
 
-    this.remove = function remove() {
-      return 'epubs=`ls -1 *.epub 2>/dev/null | wc -l`; if [ $epubs != 0 ]; then rm *.epub; fi'
-    }
+  _get(key) {
+    return this.options[key]
+  }
 
-    this.compile = function compile() {
-      return [
-        `zip -X0 ${this._get('bookpath')} ./mimetype`,
-        `zip -X9Dr ${this._get('bookpath')} ./META-INF -x *.DS_Store`,
-        `zip -X9Dr ${this._get('bookpath')} ./OPS -x *.DS_Store`
-      ].join(' && ')
-    }
+  static remove() {
+    return 'epubs=`ls -1 *.epub 2>/dev/null | wc -l`; if [ $epubs != 0 ]; then rm *.epub; fi'
+  }
 
-    this.validate = function validate() {
-      return `java -jar ${path.resolve(__dirname, './vendor/epubcheck.jar')} -e ${this._get('bookname')}` // -e: only show fatal errors
-    }
+  static report(err, stdout, stderr, reject) {
+    if (err) { reject(err) }
+    if (stderr !== '') { reject(new Error(stderr)) }
+    if (stdout !== '') { console.log(stdout) } // eslint-disable-line no-console
+  }
 
-    this.report = function report(err, stdout, stderr, reject) {
-      if (err) { reject(err) }
-      if (stderr !== '') { reject(new Error(stderr)) }
-      if (stdout !== '') { console.log(stdout) }
-    }
+  compile() {
+    return [
+      `zip -X0 ${this._get('bookpath')} ./mimetype`,
+      `zip -X9Dr ${this._get('bookpath')} ./META-INF -x *.DS_Store`,
+      `zip -X9Dr ${this._get('bookpath')} ./OPS -x *.DS_Store`
+    ].join(' && ')
+  }
 
-    this.run = function(cmd, dir) {
-      return new Promise((resolve, reject) => {
-        exec(this[cmd](), { cwd: dir }, (err, stdout, stderr) => {
-          this.report(err, stdout, stderr, reject)
-          resolve()
-        })
+  validate() {
+    return [
+      `java -jar ${path.resolve(__dirname, 'vendor/epubcheck.jar')}`,
+      this._get('flags').join(' '),
+      this._get('bookname')
+    ].join(' ')
+  }
+
+  run(cmd, dir) {
+    return new Promise((resolve, reject) => {
+      exec(this[cmd](), { cwd: dir }, (err, stdout, stderr) => {
+        this.report(err, stdout, stderr, reject)
+        resolve()
       })
-    }
+    })
+  }
 
-    this.conditionally = function conditionally(test, { cmd, dir }) {
-      return new Promise((resolve /* , reject */) => {
-        if (test) {
-          this.run(cmd, dir).then(resolve)
-        } else {
-          resolve()
-        }
-      })
-    }
+  conditionally(test, { cmd, dir }) {
+    return new Promise((resolve/* , reject */) => {
+      if (test) {
+        this.run(cmd, dir).then(resolve)
+      } else {
+        resolve()
+      }
+    })
+  }
 
-    this.create = function create({ ...args }) {
-      const { input, output, clean } = args
-      this._set('input', input)
-      this._set('output', output)
-      this._set('clean', clean)
-      this._set('modified', new Date().toISOString())
-      this._set('bookname', `${this._get('modified')}.epub`)
-      this._set('bookpath', `"${path.resolve(__dirname, this._get('input'), '../', this._get('bookname'))}"`)
-      return new Promise(resolve/* , reject */ =>
-        this.conditionally(clean, { cmd: 'remove', output })
-        .then(() => this.run('compile', input))
-        .then(() => this.run('validate', output))
-        .catch(err => console.error(err))
-        .then(resolve)
-      )
-    }
-
+  create({ ...args }) {
+    Object.assign(this.options, args)
+    const required = ['input', 'output', 'bookname']
+    required.forEach((_) => {
+      if (!this.options[_] || !{}.hasOwnProperty.call(this.options, _)) {
+        throw new Error(`Missing required argument: \`${_}\``)
+      }
+    })
+    this._set('modified', new Date().toISOString())
+    this._set('bookname', `${this._get('modified')}.epub`)
+    this._set('bookpath', `"${path.resolve(this._get('output'), this._get('bookname'))}"`)
+    return new Promise(resolve/* , reject */ =>
+      this.conditionally(this._get('clean'), { cmd: 'remove', output: this._get('output') })
+      .then(() => this.run('compile', { output: this._get('input') }))
+      .then(() => this.run('validate', { output: this._get('output') }))
+      .catch(err => console.error(err)) // eslint-disable-line no-console
+      .then(resolve)
+    )
   }
 }
 
