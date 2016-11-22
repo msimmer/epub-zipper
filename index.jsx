2021e8e0ce255a125df03db666643e3abc469e0e
Maxwell Simmer <maxwell.simmer@gmail.com>
2016-11-22 12:29:56 +0100

    Add test

--- index.jsx
+++ index.jsx
@@ -7,7 +7,9 @@ class Epub {
     this.options = {
       input: null,
       output: null,
+      modified: null,
       bookname: null,
+      bookpath: null,
       clean: true,
       flags: ['-e']
     }
@@ -22,11 +24,15 @@ class Epub {
     return this.options[key]
   }
 
-  static remove() {
-    return 'epubs=`ls -1 *.epub 2>/dev/null | wc -l`; if [ $epubs != 0 ]; then rm *.epub; fi'
+  remove() {
+    return [
+      `epubs=\`ls -1 ${this._get('output')}/*.epub 2>/dev/null | wc -l\`;`,
+      `if [ $epubs != 0 ]; then rm ${this._get('output')}/*.epub; fi`
+    ].join(' ')
+
   }
 
-  static report(err, stdout, stderr, reject) {
+  report(err, stdout, stderr, reject) {
     if (err) { reject(err) }
     if (stderr !== '') { reject(new Error(stderr)) }
     if (stdout !== '') { console.log(stdout) } // eslint-disable-line no-console
@@ -57,10 +63,10 @@ class Epub {
     })
   }
 
-  conditionally(test, { cmd, dir }) {
+  conditionally(test, callback) {
     return new Promise((resolve/* , reject */) => {
       if (test) {
-        this.run(cmd, dir).then(resolve)
+        return callback.then(resolve)
       } else {
         resolve()
       }
@@ -79,14 +85,15 @@ class Epub {
     this._set('bookname', `${this._get('modified')}.epub`)
     this._set('bookpath', `"${path.resolve(this._get('output'), this._get('bookname'))}"`)
     return new Promise(resolve/* , reject */ =>
-      this.conditionally(this._get('clean'), { cmd: 'remove', output: this._get('output') })
-      .then(() => this.run('compile', { output: this._get('input') }))
-      .then(() => this.run('validate', { output: this._get('output') }))
+      this.conditionally(this._get('clean'), this.run('remove', this._get('output') ))
+      .then(() => this.run('compile', this._get('input') ))
+      .then(() => this.run('validate', this._get('output') ))
       .catch(err => console.error(err)) // eslint-disable-line no-console
       .then(resolve)
     )
   }
 }
+
 
 const epub = new Epub()
 export default epub
--- test.js
+++ test.js
@@ -1,2 +1,27 @@
 
 import test from 'ava'
+import epub from './index'
+import path from 'path'
+
+test('It conditionally executes a promise callback', t => {
+  t.plan(2)
+
+  const callback = Promise.resolve(1)
+  const conditional = c => {
+    return new Promise((resolve, reject) => {
+      if (c) {
+        return callback.then(resolve)
+      } else {
+        resolve(0)
+      }
+    })
+  }
+
+  const truthy = true
+  const falsey = false
+
+  return conditional(truthy)
+  .then(async result => t.is(await result, 1))
+  .then(() => conditional(falsey))
+  .then(async result => t.is(await result, 0))
+})
